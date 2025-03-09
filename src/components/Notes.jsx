3314d14ef1cfcf22daca4665784b5b6b3fe6e319
Padam Gadshila <padamgadshila17@gmail.com>
2025-03-09 21:37:10 +0530

    Fixed No title and content

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -28,11 +28,13 @@ export default function Notes({ data, status }) {
     <div
       className={`${
         status ? "bg-red-500 text-white" : "bg-gray-200 text-black"
-      } w-full flex flex-col p-3 rounded-md h-[103px] my-2 select-auto pointer-events-none transition-all duration-300 ease-in-out`}
+      } w-full flex flex-col p-3 rounded-md my-2 select-auto pointer-events-none transition-all duration-300 ease-in-out`}
     >
-      <h1 className="text-2xl font-bold">{data.title}</h1>
-      <span className="text-xl inline-block w-full text-ellipsis whitespace-nowrap overflow-hidden">
-        {data.content}
+      <h1 className="text-2xl font-bold h-[30px]">
+        {data.title ? data.title : "No Title"}
+      </h1>
+      <span className="text-xl h-[30px] inline-block w-full text-ellipsis whitespace-nowrap overflow-hidden">
+        {data.content ? data.content : "No Content"}
       </span>
       <span className="text-sm">{formatDate(data.date)}</span>
     </div>
